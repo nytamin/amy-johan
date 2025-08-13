7636043649a874f46e7721a31abd9cd80b9a72b9
Johan Nyman <johan@nytamin.se>
2025-08-13 08:50:12 +0200

    bordsplacering

--- app/bordsplacering.tsx
+++ app/bordsplacering.tsx
@@ -3,13 +3,11 @@ import React from "react";
 export default function Bordsplacering() {
   return (
     <>
-      <div className="m-4 text-left text-section mx-auto max-w-[40em]">
-        <h2 className="text-xl my-1">Bordsplacering</h2>
+      <div className="m-4 text-left text-section mx-auto ">
+        <h2 className="text-xl text-center my-1">Bordsplacering</h2>
 
         <div>
-          <div>
-            <img src="quiz/bordsplacering.jpg" alt="Bordsplacering" />
-          </div>
+          <img src="/bordsplacering.png" alt="Bordsplacering" />
         </div>
       </div>
       <div className="m-4 text-left text-section mx-auto max-w-[40em]"></div>
